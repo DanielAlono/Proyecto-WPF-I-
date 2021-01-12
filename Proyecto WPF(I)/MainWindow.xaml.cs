7fab50f04b3834e1dcbe1b0cd0fa607c11087ade
Daniel Alonso <de.alonso@iesdoctorbalmis.com>
2021-01-12 07:40:44 +0100

    Ventana gestionar casi realizada

--- Proyecto WPF(I)/MainWindow.xaml.cs
+++ Proyecto WPF(I)/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Proyecto_WPF_I_
 {
@@ -20,9 +13,94 @@ namespace Proyecto_WPF_I_
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Pelicula> collection;
+        Pelicula pelicula;
         public MainWindow()
         {
             InitializeComponent();
+            collection = new ObservableCollection<Pelicula>();
+            nombresComboBox();
+            facilRadioButton.IsChecked = true; //Fácil por defecto
+            peliculasListBox.DataContext = collection;
+        }
+
+        private void añadirButton_Click(object sender, RoutedEventArgs e)
+        {
+            pelicula = new Pelicula();
+            pelicula.Titulo = tituloTextBox.Text;
+            pelicula.Imagen = imagenTextBox.Text;
+            pelicula.Pista = pistaTextBox.Text;
+            pelicula.Dificultad = dificultad();
+            pelicula.Genero = opcionesComboBox.Text;
+            collection.Add(pelicula);
+            reiniciarCasillas();
+        }
+        private string dificultad()
+        {
+            if ((bool)facilRadioButton.IsChecked)
+            {
+                return "Facil";
+            }
+            else if ((bool)normalRadioButton.IsChecked)
+            {
+                return "Normal";
+            }
+            else if ((bool)dificilRadioButton.IsChecked)
+            {
+                return "Dificil";
+            }
+            return null;
+        }
+        private void eliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            collection.Remove((Pelicula)peliculasListBox.SelectedItem);
+        }
+
+        private void reiniciarCasillas()
+        {
+            tituloTextBox.Text = "";
+            imagenTextBox.Text = "";
+            pistaTextBox.Text = "";
+        }
+        private void nombresComboBox()
+        {
+            List<String> generos = new List<string>() { "Comedia", "Drama", "Acción", "Terror", "Ciencia Ficción" };
+            opcionesComboBox.ItemsSource = generos;
+            opcionesComboBox.SelectedIndex = 0; //dejamos el primer item de la lista seleccionado
+        }
+
+        private void cargarJSONButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string json = File.OpenText(openFileDialog.FileName).ReadToEnd();
+                ObservableCollection<Pelicula> peliculas = JsonConvert.DeserializeObject<ObservableCollection<Pelicula>>(json);
+
+                foreach (Pelicula p in peliculas)
+                {
+                    collection.Add(p);
+                }
+            }
+        }
+
+        private void guardarJSONButton_Click(object sender, RoutedEventArgs e)
+        {
+            string peliculasJson = JsonConvert.SerializeObject(collection);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, peliculasJson);
+            }
+        }
+
+        private void examinarButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == true)
+            {
+                imagenTextBox.Text = openFileDialog.FileName;
+            }
         }
     }
 }
---
+++ Proyecto WPF(I)/Pelicula.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel;
+
+namespace Proyecto_WPF_I_
+{
+
+    class Pelicula : INotifyPropertyChanged
+    {
+        private string _titulo;
+
+        public string Titulo
+        {
+            get { return this._titulo; }
+            set
+            {
+                if (this._titulo != value)
+                {
+                    this._titulo = value;
+                    this.NotifyPropertyChanged("Titulo");
+                }
+            }
+        }
+
+        private string _imagen;
+
+        public string Imagen
+        {
+            get { return this._imagen; }
+            set
+            {
+                if (this._imagen != value)
+                {
+                    this._imagen = value;
+                    this.NotifyPropertyChanged("Imagen");
+                }
+            }
+        }
+
+        private string _pista;
+
+        public string Pista
+        {
+            get { return this._pista; }
+            set
+            {
+                if (this._pista != value)
+                {
+                    this._pista = value;
+                    this.NotifyPropertyChanged("Pista");
+                }
+            }
+        }
+
+        private string _dificultad;
+
+        public string Dificultad
+        {
+            get { return this._dificultad; }
+            set
+            {
+                if (this._dificultad != value)
+                {
+                    this._dificultad = value;
+                    this.NotifyPropertyChanged("Dificultad");
+                }
+            }
+        }
+
+        private string _genero;
+
+        public string Genero
+        {
+            get { return this._genero; }
+            set
+            {
+                if (this._genero != value)
+                {
+                    this._genero = value;
+                    this.NotifyPropertyChanged("Genero");
+                }
+            }
+        }
+
+        public Pelicula() { }
+
+        public Pelicula(string titulo, string imagen, string pista, string dificultad, string genero)
+        {
+            Titulo = titulo;
+            Imagen = imagen;
+            Pista = pista;
+            Dificultad = dificultad;
+            Genero = genero;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
